7e4e3733801f7d06fd9bb9c95eecf75cb3088ab8
sasha <sasha.mihalache@gmail.com>
2018-08-11 16:19:43 +0300

    fix to connectionString

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const imageCtrl = require('./controllers/ImageCtrl');
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   }
 });
